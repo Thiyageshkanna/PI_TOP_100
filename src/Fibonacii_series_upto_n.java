64af0dd8a47839b8d93bcee28e757edeaf929e25
thiyagesh kanna <thiyagesh14092000@gmail.com>
2021-10-22 18:20:42 +0530

    Fibonacii_series_upto_n.

--- src/Fibonacii_series_upto_n.java
+++ src/Fibonacii_series_upto_n.java
@@ -6,13 +6,18 @@ public class Fibonacii_series_upto_n {
         int number =scn.nextInt();
         int start=0;
         int end=1;
-        System.out.print(start+" "+end+" ");
-        for(int i=2;i<=number;i++){
-            int result=start+end;
-            System.out.print(result + " ");
-            int temp=start;
-            start=end;
-            end=result;
+        int result=0;
+        if(number>0){
+            System.out.print(start+" "+end+" ");
+            while(number>result){
+                result=start+end;
+                if (number>=result){
+                    System.out.print(result+" ");
+                }
+                int temp=start;
+                start=end;
+                end=result;
+            }
         }
     }
 
